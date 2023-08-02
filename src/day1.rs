c954f8dd3c7dc5271bbfd68cee755e624820433e
didadadida93 <didadadida93@gmail.com>
2023-08-02 19:37:22 +0700

    Finished solution day 1

---
+++ src/day1.rs
@@ -0,0 +1,36 @@
+use std::fs;
+
+pub fn solve() -> (u32, u32) {
+    let largest = fs::read_to_string("./input/day-1.txt")
+        .expect("Failed to read day-1.txt file")
+        .split("\n\n")
+        .map(|item| {
+            item.lines()
+                .map(|e| {
+                    e.trim()
+                        .parse::<u32>()
+                        .expect("Failed to parse into number")
+                })
+                .sum::<u32>()
+        })
+        .max()
+        .expect("Failed to find max value");
+
+    let mut v: Vec<_> = fs::read_to_string("./input/day-1.txt")
+        .expect("Failed to read day-1.txt file")
+        .split("\n\n")
+        .map(|item| {
+            item.lines()
+                .map(|e| {
+                    e.trim()
+                        .parse::<u32>()
+                        .expect("Failed to parse into number")
+                })
+                .sum::<u32>()
+        })
+        .collect();
+    v.sort_unstable();
+    let total_top_3: u32 = v.into_iter().rev().take(3).sum();
+
+    (largest, total_top_3)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use aoc::day1;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (largest, top3) = day1::solve();
+    println!("{}", largest);
+    println!("{}", top3);
+    Ok(())
 }
