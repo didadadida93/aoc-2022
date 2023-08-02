0db9d091de2a2cfc229c804b7815ca360ad61ad6
didadadida93 <didadadida93@gmail.com>
2023-08-02 19:39:22 +0700

    Remove unnecessary dep

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use aoc::day1;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     let (largest, top3) = day1::solve();
     println!("{}", largest);
     println!("{}", top3);
-    Ok(())
 }
